c7d84f05bdbad5f9abcd4e988e88faf6ddb79e54
Interware <interware@localhost.localdomain>
2018-02-19 16:18:56 -0600

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to apperick
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/apperick/core_test.clj
@@ -0,0 +1,7 @@
+(ns apperick.core-test
+  (:require [clojure.test :refer :all]
+            [apperick.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
